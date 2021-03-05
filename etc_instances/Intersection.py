1587c059882d38ea0b1301f6a43d1977da645bab
nicholasid7 <elkinfor@gmail.com>
2021-03-05 22:03:14 +0500

    Start2

--- etc_instances/Intersection.py
+++ etc_instances/Intersection.py
@@ -1,7 +1,13 @@
 # Instance 1: Intersections of sets
+import numpy as np
 
 a = [1, 2, 4, 6, 8, 10, 12]
 b = [2, 3, 4, 8]
 
-print(a)
+# result [2,4,8]
+max_min_ab = max(min(a), min(b))
+min_max_ab = min(max(a), max(b))
+print(max_min_ab)
+print(min_max_ab)
+
 
